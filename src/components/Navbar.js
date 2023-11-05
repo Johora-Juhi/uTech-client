059a71726ef81cc5a2b8392c729e485638f12f43
Johora-Juhi <zohrajuhi1012@gmail.com>
2023-11-05 17:42:02 +0600

    top rated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { BsFillCartFill } from "react-icons/bs";
 import { IoIosListBox } from "react-icons/io";
 
 const Navbar = () => {
+  const { pathname } = useLocation();
+  const activePage= "bg-indigo-500 text-white px-2 pb-1 rounded"
+
   return (
     <nav className="h-14 bg-indigo-200 rounded-full m-2 max-w-7xl mx-auto px-5">
       <ul className="h-full  mx-auto flex justify-between items-center gap-3 font-semibold text-indigo-900">
         <h1 className="flex-1">Moon Tech</h1>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/" className={`${pathname.includes("") ? activePage : null}`}>Home</Link>
         </li>
         <li>
-          <Link to="/top-rated">Top Rated</Link>
+          <Link to="/top-rated" className={`${pathname.includes("top-rated") ? activePage : null}`}>Top Rated</Link>
         </li>
         <li>
           <Link to="/about">About</Link>

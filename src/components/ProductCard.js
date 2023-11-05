ce2ee8834deffdbaa7e70e89145eb4cc22026ab4
Johora-Juhi <zohrajuhi1012@gmail.com>
2023-11-05 14:53:13 +0600

    products filter

---
+++ src/components/ProductCard.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { BiListPlus } from "react-icons/bi";
+import { useLocation } from "react-router-dom";
+
+const ProductCard = ({ product }) => {
+
+  const { pathname } = useLocation();
+  return (
+    <div
+      className="shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900 relative"
+      key={product._id}
+    >
+      {pathname.includes("cart") && (
+        <div className="bg-indigo-500 grid place-items-center h-8 w-8 absolute text-white rounded-full top-2 right-2">
+          <p>{product.quantity}</p>
+        </div>
+      )}
+      <div className="h-52 w-52 mx-auto">
+        <img src={product.image} alt={product.model} />
+      </div>
+      <h1 className="font-bold text-center">{product.model}</h1>
+      <p className="text-center font-semibold mb-3">Rating: {product.rating}</p>
+      <div className=" flex-1">
+        <ul className="space-y-2">
+          {product.keyFeature.map((feature) => {
+            return <li className="text-sm ">{feature}</li>;
+          })}
+        </ul>
+      </div>
+      <div className="flex gap-2 mt-5">
+        {!pathname.includes("cart") && (
+          <button
+            // onClick={() => addToCart(product))}
+            className="bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold"
+          >
+            Add to cart
+          </button>
+        )}
+        {pathname.includes("cart") && (
+          <button
+            // onClick={() => removeFromCart(product))}
+            className="bg-red-500 rounded-full py-1 px-2 flex-1 text-white text-bold"
+          >
+            Remove from cart
+          </button>
+        )}
+        {!pathname.includes("cart") && (
+          <button
+            title="Add to wishlist"
+            className="bg-indigo-500  py-1 px-2 rounded-full"
+          >
+            <BiListPlus className="text-white" />
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
---
+++ src/pages/Home.js
@@ -0,0 +1,135 @@
+import React, { useEffect, useState } from "react";
+import ProductCard from "../components/ProductCard";
+import { useQuery } from "@tanstack/react-query";
+
+const Home = () => {
+
+  const [size, setSize] = useState(8);
+  const [page, setPage] = useState(0);
+
+  const { data: productsData = { products: [], count: 0 }, refetch, isError, isLoading } = useQuery({
+    queryKey: ["products", size, page],
+    queryFn: async () => {
+      const res = await fetch(`http://localhost:5000/products?page=${page}&size=${size}`);
+      const data = await res.json();
+      return data;
+    }
+  });
+  
+  const { products, count } = productsData;
+  const pages = Math.ceil(count / size);
+  console.log(products);
+  const handleSelectChange = (event) => {
+    const selectedValue = parseInt(event.target.value, 10);
+    setSize(selectedValue);
+    setPage(0)
+  };
+  const [stock, setStock] = useState(false);
+  const [brands, setBrands] = useState([]);
+
+  const toggleStock = () => {
+    setStock(!stock);
+    console.log(stock);
+  };
+
+  const toggleBrand = (brand) => {
+    if (brands.includes(brand)) {
+      setBrands(brands.filter((b) => b !== brand));
+    } else {
+      setBrands([...brands, brand]);
+    }
+  };
+  const activeClass = "text-white  bg-indigo-500 border-white";
+
+  let content;
+
+  if (isLoading) {
+    content = <h1>Loading.....</h1>;
+  }
+
+  if (isError) {
+    content = <h1>Something went wrong</h1>;
+  }
+
+  if (products.length) {
+    content = products.map((product) => (
+      <ProductCard key={product._id} product={product}></ProductCard>
+    ));
+  }
+
+  if (products.length && (stock || brands.length)) {
+    content = products
+      .filter((product) => {
+        if (stock) {
+          return product.status === true;
+        }
+        return product;
+      })
+      .filter((product) => {
+        if (brands.length) {
+          return brands.includes(product.brand);
+        }
+        return product;
+      })
+      .map((product) => (
+        <ProductCard key={product._id} product={product}></ProductCard>
+      ));
+  }
+  return (
+    <div className="max-w-7xl gap-14 mx-auto my-10">
+      <div className="mb-10 flex justify-end gap-5">
+        <button
+         onClick={toggleStock}
+          className={`border px-3 py-2 rounded-full font-semibold ${
+            stock ? activeClass : null
+           } `
+          }
+        >
+          In Stock
+        </button>
+        <button
+          onClick={() => toggleBrand("amd")}
+          className={`border px-3 py-2 rounded-full font-semibold ${
+            brands.includes("amd") ? activeClass : null
+              }`}
+        >
+          AMD
+        </button>
+        <button
+          onClick={() => toggleBrand("intel")}
+          className={`border px-3 py-2 rounded-full font-semibold ${
+            brands.includes("intel") ? activeClass : null
+          }`}
+        >
+          Intel
+        </button>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10">
+        {content}
+      </div>
+      <div className="pagination d-flex justify-content-center ">
+            
+            {[...Array(pages).keys()].map((number) => (
+              <button
+                key={number}
+                className={page === number ? "w-[26px] h-[26px] bg-indigo-500 text-white" : "w-[26px] h-[26px] bg-gray-300"}
+                onClick={() => setPage(number)}
+              >
+                {number+1}
+              </button>
+            ))}
+            <select onChange={handleSelectChange} className=" border border-grey-300 h-[26px]">
+              
+              <option value="8" selected>8</option>
+              <option value="12">
+                12
+              </option>1
+              <option value="16">16</option>
+              <option value="20">20</option>
+            </select>
+          </div>
+    </div>
+  );
+};
+
+export default Home;
